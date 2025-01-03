039a66fac2e1698dc4946cfb20ee34dac470fc9b
desutech <desaleyyazezew@gmail.com>
2025-01-03 12:40:10 +0300

    div

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
-import { div } from "three/tsl";
+// import { div } from "three/tsl";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");

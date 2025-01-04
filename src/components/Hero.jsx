45ae4c86d1f70d512ffc514293ac988869634f49
desutech <desaleyyazezew@gmail.com>
2025-01-04 05:16:56 +0300

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <div className="absolute inset-0">
+      <div className="absolute inset-0 hidden md:block">
       <ComputersCanvas />
       </div>
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
